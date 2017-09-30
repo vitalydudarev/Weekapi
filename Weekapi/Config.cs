feb8b91f89bbcdcd0d7a987d7036d8a9a2a14f3b
Vitaly Dudarev <vitalydudarev@gmail.com>
2017-09-30 18:02:09 +0300

    Added Config.

---
+++ Weekapi/Config.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Weekapi
+{
+    public class Config
+    {
+        public string VkAccessToken { get; set; }
+        public string VkApiVersion { get; set; }
+        public string VkUri { get; set; }
+
+        public void CheckValid()
+        {
+            if (string.IsNullOrEmpty(VkAccessToken))
+                throw new Exception("VK Acces Token is not defined.");
+            
+            if (string.IsNullOrEmpty(VkApiVersion))
+                throw new Exception("VK API Version is not defined.");
+            
+            if (string.IsNullOrEmpty(VkUri))
+                throw new Exception("VK Uri is not defined.");
+        }
+    }
+}
